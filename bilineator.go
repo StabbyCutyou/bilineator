f589e3d31ff497a7f4da3c3eacefa05885fa0cd4
StabbyCutyou <smattkelly@gmail.com>
2015-10-15 22:51:33 -0400

    docs

--- bilineator.go
+++ bilineator.go
@@ -1,5 +1,7 @@
 package bilineator
 
+// Bilineator represents a set of values and the result of a function F with those
+// inputs, such that it can derive the result of F over any inputs.
 type Bilineator struct {
 	X1 float64
 	Y1 float64
@@ -13,7 +15,11 @@ type Bilineator struct {
 	Q22 float64
 }
 
+// Bilineate will let you determine the result of a function F over inputs X and Y
+// based on the given inputs in the Bilineator
 func (b *Bilineator) Bilineate(x float64, y float64) float64 {
+	// I ran some benchmarks - I can cut out the allocations and shave 1ns per op it seems.
+	// Doesn't seem worth it at the time.
 	fxy1 := (((b.X2 - x) / (b.X2 - b.X1)) * b.Q11) + ((x - b.X1) / (b.X2 - b.X1) * b.Q21)
 	fxy2 := (((b.X2 - x) / (b.X2 - b.X1)) * b.Q12) + ((x - b.X1) / (b.X2 - b.X1) * b.Q22)
 
