5b2933e99719354faf0f3327929607cfc9a4a3ec
StabbyCutyou <smattkelly@gmail.com>
2017-09-09 21:43:43 -0400

    Minor update

--- bilineator.go
+++ bilineator.go
@@ -22,8 +22,5 @@ func (b *Bilineator) Bilineate(x float64, y float64) float64 {
 	// Doesn't seem worth it at the time.
 	fxy1 := (((b.X2 - x) / (b.X2 - b.X1)) * b.Q11) + ((x - b.X1) / (b.X2 - b.X1) * b.Q21)
 	fxy2 := (((b.X2 - x) / (b.X2 - b.X1)) * b.Q12) + ((x - b.X1) / (b.X2 - b.X1) * b.Q22)
-
-	fxy := (((b.Y2 - y) / (b.Y2 - b.Y1)) * fxy1) + ((y - b.Y1) / (b.Y2 - b.Y1) * fxy2)
-
-	return fxy
+	return (((b.Y2 - y) / (b.Y2 - b.Y1)) * fxy1) + ((y - b.Y1) / (b.Y2 - b.Y1) * fxy2)
 }
